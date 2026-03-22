32aaa081f95c4f6363d538a6627bff5625c631d5
Yuvraj <yuvrajtiwari.work@gmail.com>
2026-03-22 19:39:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  // Ye line aapke URL ko fix karegi
   base: "/", 
   plugins: [react()],
   resolve: {
